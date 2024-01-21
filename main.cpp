c1ec0d931cbb267e36fbc3c8b6e736b73b1f7b7f
Dmytro990 <156248133+Dmytro990@users.noreply.github.com>
2024-01-21 19:03:37 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,39 +2,99 @@
 #include <cstdlib>
 #include <ctime>
 
-#define ARRAY_SIZE 10
-#define RANGE_MIN 1
-#define RANGE_MAX 100
+const int ARRAY_SIZE = 10;
 
 struct Range {
     int x;
     int y;
 };
 
-void fillArray(int arr[], int size, const Range& range) {
-    std::srand(std::time(0)); 
+void wypelnijTab(int arr[], int size) {
+    std::srand(std::time(nullptr));
 
     for (int i = 0; i < size; ++i) {
-        arr[i] = std::rand() % (range.y - range.x + 1) + range.x;
+        arr[i] = std::rand() % 100; 
+    }
+}
+
+void drukujTab(const int arr[], int size) {
+    std::cout << "Tablica: ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+void sortowanie_babelkowe(int arr[], int size) {
+    for (int i = 0; i < size - 1; ++i) {
+        for (int j = 0; j < size - i - 1; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                std::swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+void merge(int arr[], int start, int mid, int end) {
+    int n1 = mid - start + 1;
+    int n2 = end - mid;
+
+    int* left = new int[n1];
+    int* right = new int[n2];
+
+    for (int i = 0; i < n1; ++i) {
+        left[i] = arr[start + i];
+    }
+    for (int j = 0; j < n2; ++j) {
+        right[j] = arr[mid + 1 + j];
+    }
+
+    int i = 0, j = 0, k = start;
+    while (i < n1 && j < n2) {
+        arr[k++] = (left[i] <= right[j]) ? left[i++] : right[j++];
+    }
+
+    while (i < n1) {
+        arr[k++] = left[i++];
+    }
+
+    while (j < n2) {
+        arr[k++] = right[j++];
+    }
+
+    delete[] left;
+    delete[] right;
+}
+
+void sortowanie_przez_scalanie(int arr[], int start, int end) {
+    if (start < end) {
+        int mid = start + (end - start) / 2;
+
+        sortowanie_przez_scalanie(arr, start, mid);
+        sortowanie_przez_scalanie(arr, mid + 1, end);
+
+        merge(arr, start, mid, end);
     }
 }
 
 int main() {
-    const int n = ARRAY_SIZE;
+    int myArray[ARRAY_SIZE];
 
-    Range range;
-    range.x = RANGE_MIN;
-    range.y = RANGE_MAX;
+    // Sortowanie bąbelkowe
+    wypelnijTab(myArray, ARRAY_SIZE);
+    drukujTab(myArray, ARRAY_SIZE);
 
-    int myArray[n];
+    sortowanie_babelkowe(myArray, ARRAY_SIZE);
 
-    fillArray(myArray, n, range);
+    drukujTab(myArray, ARRAY_SIZE);
 
-    std::cout << "Filled Array: ";
-    for (int i = 0; i < n; ++i) {
-        std::cout << myArray[i] << " ";
-    }
-    std::cout << std::endl;
+    // Sortowanie przez scalanie
+    wypelnijTab(myArray, ARRAY_SIZE);
+    drukujTab(myArray, ARRAY_SIZE);
+
+    sortowanie_przez_scalanie(myArray, 0, ARRAY_SIZE - 1);
+
+    drukujTab(myArray, ARRAY_SIZE);
 
     return 0;
 }
